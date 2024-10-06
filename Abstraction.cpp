c60601cb9eea66e8ff8a2ccb6c47116d363b5ff7
MDerin1834 <mustafa.derin@sabanciuniv.edu>
2024-10-06 18:15:43 +0300

    Add files via upload

---
+++ Abstraction.cpp
@@ -0,0 +1,50 @@
+﻿// Abstraction.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+using namespace std;
+
+class Hayvan {
+    virtual void sesCal() = 0;
+};
+class kedi: public Hayvan {
+
+    void sesCal() {
+        cout << "Miyav";
+    }
+};
+class kopek :public Hayvan {
+public:
+    void sesCal() {
+        cout << "hav hav";
+    }
+}; 
+class ordek:public Hayvan {
+public:
+    void sesCal() {
+        cout << "vak vak";
+    }
+};
+
+void tumsescal(Hayvan* hPtr) {
+    hPtr->sesCal();
+}
+int main()
+{
+    kopek kopek;
+    kedi kedi;
+    ordek ordek;
+    tumsescal(&ordek);
+
+}
+
+// Programı çalıştır: Ctrl + F5 veya Hata Ayıkla > Hata Ayıklamadan Başlat menüsü
+// Programda hata ayıkla: F5 veya Hata Ayıkla > Hata Ayıklamayı Başlat menüsü
+
+// Kullanmaya Başlama İpuçları: 
+//   1. Dosyaları eklemek/yönetmek için Çözüm Gezgini penceresini kullanın
+//   2. Kaynak denetimine bağlanmak için Takım Gezgini penceresini kullanın
+//   3. Derleme çıktısını ve diğer iletileri görmek için Çıktı penceresini kullanın
+//   4. Hataları görüntülemek için Hata Listesi penceresini kullanın
+//   5. Yeni kod dosyaları oluşturmak için Projeye Git > Yeni Öğe ekle veya varolan kod dosyalarını projeye eklemek için Proje > Var Olan Öğeyi Ekle adımlarını izleyin
+//   6. Bu projeyi daha sonra yeniden açmak için Dosya > Aç > Proje'ye gidip .sln uzantılı dosyayı seçin
